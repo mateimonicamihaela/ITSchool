8d5e288e14a4222bad8d4cb85c612ff9cb3251c8
Monica-Mihaela Matei <matei.monicamihaela@yahoo.com>
2025-06-25 20:20:58 +0300

    Added updates to exercise

--- scripts/Python/joc.py
+++ scripts/Python/joc.py
@@ -1,11 +1,19 @@
 import random
 import os
+import json
 
-min_val=int(os.getenv("MIN_VAL",1))
-max_val=int(os.getenv("MAX_VAL", 100))
+
+FILE_ISTORIE = "istorie_joc.json"
+if os.path.exists(FILE_ISTORIE):
+    with open(FILE_ISTORIE, "r") as f:
+        istorie_joc = json.load(f)
+else:
+    istorie_joc = {}
+
+min_val = int(os.getenv("MIN_VAL", 1))
+max_val = int(os.getenv("MAX_VAL", 100))
 
 numar_generat = random.randint(min_val, max_val)
-
 def citeste_intreg(min_arg, max_arg = 100):
     while True:
         try:
@@ -14,9 +22,10 @@ def citeste_intreg(min_arg, max_arg = 100):
                 return valoare_introdusa #return inchide functia
             else:
                 print(f"Numarul introdus trebuie sa fie intre {min_arg} si {max_arg}")
-        except Exception as eroare:
+        except Exception as eroare: 
             print(eroare)
             continue
+
 tentativa = 0
 
 while True:
@@ -29,5 +38,18 @@ while True:
     else:
         print(f"Nu ai ghicit numarul. Aceasta este incercarea numarul {tentativa}")
         break
-
 print(f"Ai ghicit numarul din a {tentativa} incercare")
+user_name = input("Introduceti numele: ")
+
+if user_name in istorie_joc:
+    incercari_anterioare = istorie_joc[user_name]
+    if incercari_anterioare > tentativa:
+        istorie_joc[user_name] = tentativa
+    print("Userul deja exista")
+else:
+    istorie_joc[user_name] = tentativa
+for user in istorie_joc:
+    print(f"{user} : {istorie_joc[user]}")
+
+with open(FILE_ISTORIE, "w") as f:
+    json.dump(istorie_joc, f)
